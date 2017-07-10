bd491e5853e2ad04771a4aa66d42593ed600cd5c
cecicosta <cecier.costa@gmail.com>
2017-07-10 00:08:03 -0300

    Uploading full project dependencies

--- ArteDraw/usercontrol/cont_propiedades.xaml.cs
+++ ArteDraw/usercontrol/cont_propiedades.xaml.cs
@@ -17,15 +17,25 @@ using System.Windows.Shapes;
 
 namespace ArteDraw
 {
+
+    public static class ExtensionMethods {
+        private static Action EmptyDelegate = delegate () { };
+
+        public static void Refresh(this UIElement uiElement) {
+            uiElement.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Render, EmptyDelegate);
+        }
+    }
     /// <summary>
     /// Interaction logic for propiedades.xaml
     /// </summary>
     public partial class propiedades : UserControl
     {
         public string Points { get; private set; }
-        Rectangle r;
         ContentControl c;
         Point mouseStartPosition;
+        Point min;
+        Point max;
+        private Polyline polyline;
 
         public propiedades()
         {
@@ -33,6 +43,12 @@ namespace ArteDraw
 
         }
 
+        /// <summary>
+        /// Event called when the buttom to create Rectangle is pressed.
+        /// it binds the callback methods to handle the drawing of the shape
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void InsertQuadStart(object sender, RoutedEventArgs e)
         {
             Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
@@ -43,42 +59,140 @@ namespace ArteDraw
         private void InsertQuadMouseDown(object sender, MouseButtonEventArgs e) {
             Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
             canvas.PreviewMouseDown -= InsertQuadMouseDown;
-            canvas.PreviewMouseUp += InsertQuadMouseUp;
-            canvas.PreviewMouseMove += InserQuadContinue;
+            canvas.PreviewMouseUp += ShapeDragFinish;
+            canvas.PreviewMouseMove += ShapeDrag;
             //Create the Rectangle and set its values
             mouseStartPosition = e.GetPosition(canvas);
-            r = new Rectangle();
-            Canvas.SetLeft(r, mouseStartPosition.X);
-            Canvas.SetTop(r, mouseStartPosition.Y);
-            r.StrokeThickness = 5;
-            r.Stroke = Brushes.Black;
-            r.Fill = Brushes.Transparent;
-            r.IsHitTestVisible = false;
-            r.RenderTransformOrigin = new Point(0.5, 0.5);
-            //Create the ContentControl and insert the Rectangle
+            //Create and init shape
+            Rectangle rect = new Rectangle();
+            Shape init = (Shape)rect;
+            StartShape(ref init, mouseStartPosition);
+
+            //Insert the ContentControl into the canvas
+            canvas.Children.Add(CreateContentControl(rect));
+        }
+        
+        /// <summary>
+        /// Event called when the buttom to create Ellipses is pressed.
+        /// it binds the callback methods to handle the drawing of the shape
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InsertCircleStart(object sender, RoutedEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            canvas.PreviewMouseDown -= InsertCircleMouseDown;
+            canvas.PreviewMouseDown += InsertCircleMouseDown;
+        }
+
+        private void InsertCircleMouseDown(object sender, MouseButtonEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            canvas.PreviewMouseDown -= InsertCircleMouseDown;
+            canvas.PreviewMouseUp += ShapeDragFinish;
+            canvas.PreviewMouseMove += ShapeDrag;
+            //Create the Rectangle and set its values
+            mouseStartPosition = e.GetPosition(canvas);
+            //Create and init shape
+            Ellipse ellipse = new Ellipse();
+            Shape init = (Shape)ellipse;
+            StartShape(ref init, mouseStartPosition);
+
+            //Insert the ContentControl into the canvas
+            canvas.Children.Add(CreateContentControl(ellipse));
+        }
+
+        private void InsertLineStart(object sender, RoutedEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            canvas.PreviewMouseLeftButtonDown -= InsertLineMouseDown;
+            canvas.PreviewMouseLeftButtonDown += InsertLineMouseDown;
+        }
+
+        private void InsertLineMouseDown(object sender, MouseButtonEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            canvas.PreviewMouseLeftButtonDown -= InsertLineMouseDown;
+            canvas.PreviewMouseLeftButtonDown += InsertLinePoints;
+            canvas.PreviewMouseRightButtonDown += FinishLine;
+
+            mouseStartPosition = e.GetPosition(canvas);
+
+            // Create a polyline
+            polyline = new Polyline();
+            Shape shape = (Shape)polyline;
+            StartShape(ref shape, new Point(0,0));
+            
+            polyline.Points.Add(mouseStartPosition);
+            canvas.Children.Add(polyline);
+
+            min = mouseStartPosition;
+            max = mouseStartPosition;
+        }
+
+        private void InsertLinePoints(object sender, MouseButtonEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            Point point = e.GetPosition(canvas);
+            polyline.Points.Add(point);
+
+            min.X = min.X > point.X ? point.X : min.X;
+            min.Y = min.Y > point.Y ? point.Y : min.Y;
+            max.X = max.X < point.X ? point.X : max.X;
+            max.Y = max.Y < point.Y ? point.Y : max.Y;
+        }
+
+        private void FinishLine(object sender, MouseButtonEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            canvas.PreviewMouseLeftButtonDown -= InsertLinePoints;
+            canvas.PreviewMouseRightButtonDown -= FinishLine;
+
+            double thickness = Math.Ceiling(polyline.StrokeThickness / 2);
+            c = new ContentControl();
+            c.Padding = new Thickness(-min.X, -min.Y, -thickness, -thickness);
+
+            Style s = (Style)FindResource("DesignerItemStyle");
+            c.Style = s;
+            
+            c.Height = max.Y - min.Y;
+            c.Width = max.X - min.X;
+            Canvas.SetTop(c, min.Y);
+            Canvas.SetLeft(c, min.X);
+            c.Content = polyline;
+            canvas.Children.Remove(polyline);
+            canvas.Children.Add(c);
+        }
+
+        private void ShapeDrag(object sender, MouseEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            c.Width = Math.Max(e.GetPosition(canvas).X - mouseStartPosition.X, 0);
+            c.Height = Math.Max(e.GetPosition(canvas).Y - mouseStartPosition.Y, 0);
+        }
+
+        private void ShapeDragFinish(object sender, MouseButtonEventArgs e) {
+            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
+            canvas.PreviewMouseUp -= ShapeDragFinish;
+            canvas.PreviewMouseMove -= ShapeDrag;
+        }
+
+        private Shape StartShape(ref Shape shape, Point position) {
+            Canvas.SetLeft(shape, position.X);
+            Canvas.SetTop(shape, position.Y);
+            shape.StrokeThickness = 3;
+            shape.Stroke = Brushes.Black;
+            shape.Fill = Brushes.Transparent;
+            shape.IsHitTestVisible = false;
+            shape.RenderTransformOrigin = new Point(0.5, 0.5);
+            return shape;
+        }
+
+        private ContentControl CreateContentControl(Shape shape) {
+            //Create the ContentControl
             c = new ContentControl();
             c.Height = 0;
             c.Width = 0;
             c.Padding = new Thickness(0);
             Canvas.SetTop(c, mouseStartPosition.Y);
             Canvas.SetLeft(c, mouseStartPosition.X);
-            c.Content = r;
+            c.Content = shape;
             Style s = (Style)FindResource("DesignerItemStyle");
             c.Style = s;
-            //Insert the ContentControl for the Rectangle into the canvas
-            canvas.Children.Add(c);
-        }
-
-        private void InserQuadContinue(object sender, MouseEventArgs e) {
-            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
-            c.Width = Math.Max(e.GetPosition(canvas).X - mouseStartPosition.X, 0);
-            c.Height = Math.Max(e.GetPosition(canvas).Y - mouseStartPosition.Y, 0);
-        }
-
-        private void InsertQuadMouseUp(object sender, MouseButtonEventArgs e) {
-            Canvas canvas = ((MainWindow)System.Windows.Application.Current.MainWindow).canvas;
-            canvas.PreviewMouseUp -= InsertQuadMouseUp;
-            canvas.PreviewMouseMove -= InserQuadContinue;
+            return c;
         }
 
         private void btn_seta_Click(object sender, RoutedEventArgs e)
